4321ff432f112bdf6fd9c593f9ede58b01e854a7
Scott <yusufs3435@gmail.com>
2023-12-21 16:23:57 +0100

    v1

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana, Roboto_Mono } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], variable: '--font-inter', });
+
+export const lusitana = Lusitana({ subsets: ['latin'],  weight: '400' });
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+
